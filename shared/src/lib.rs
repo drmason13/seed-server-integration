1e68247d3f62e6741378aba51a61dc00d499025f
David Mason <drmason13@users.noreply.github.com>
2020-02-04 01:30:31 +0000

    chore: clean up unused code

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -9,38 +9,3 @@ pub struct Data {
     pub val: i8,
     pub text: String,
 }
-
-// all the newtypes proved a hassle so just using primitives for now
-// trait Validate<'a>: Sized {
-//     fn validate(unvalidated: &'a str) -> Result<Self, &'a str>;
-// }
-// 
-// impl<'a> Validate<'a> for Val {
-//     fn validate(unvalidated: &'a str) -> Result<Self, &'a str> {
-//         match unvalidated.parse::<i8>() {
-//             Ok(valid) => Ok(valid),
-//             _ => Err(unvalidated),
-//         }
-//     }
-// }
-// 
-// #[cfg(feature = "rocket")]
-// impl<'v> FromFormValue<'v> for Val {
-//     type Error = &'v RawStr;
-// 
-//     fn from_form_value(form_value: &'v RawStr) -> Result<Self, &'v RawStr> {
-//         Self::validate(form_value.as_str()).map_err(RawStr::from_str)
-//     }
-// }
-// 
-// #[cfg(feature = "rocket")]
-// impl<'v> FromFormValue<'v> for Text {
-//     type Error = &'v RawStr;
-// 
-//     fn from_form_value(form_value: &'v RawStr) -> Result<Self, &'v RawStr> {
-//         match form_value.len() {
-//             len if len < 100 => Ok(form_value.to_string()),
-//             _ => Err(form_value),
-//         }
-//     }
-// }
